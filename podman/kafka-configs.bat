0149928bad6e4ef836eb8918e1efcba9b02aad06
Martin H Clausen <mhh.clausen@gmail.com>
2026-05-03 21:33:38 +0200

    Podman config

--- podman/kafka-configs.bat
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --network host confluentinc/cp-kafka /bin/kafka-configs --bootstrap-server localhost:29092 %*
--- podman/kafka-log-dirs.bat
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --network host confluentinc/cp-kafka /bin/kafka-log-dirs --bootstrap-server localhost:29092 %*
